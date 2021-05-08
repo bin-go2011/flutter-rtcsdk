3e039dd69294a363b09f9849302f501c217222c1
abc <a@b.c>
2021-05-09 00:51:36 +0800

    add windows desktop support

---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+#include "generated_plugin_registrant.h"
+
+#include <flutter_rtcsdk/flutter_rtcsdk_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FlutterRtcsdkPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterRtcsdkPlugin"));
+}
